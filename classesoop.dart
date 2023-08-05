1d40101f368364e96a1084c0c46a7b03d8fe7706
luisfelipegraciolli <luisgraciolli@id.uff.br>
2023-08-05 16:39:30 -0300

    testing

---
+++ classesoop.dart
@@ -0,0 +1,8 @@
+void main(){
+    //this will be written at a machine running linux and will serve both as a test 
+    // and a learning file like the others have been so far
+
+    class MyClass(){
+        //do code here
+    }
+}
