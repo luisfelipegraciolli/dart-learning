10bacaa5711591cbeb13a30582059ac3e2900df9
luisfelipegraciolli <luisgraciolli@id.uff.br>
2023-08-05 16:35:52 -0300

    another test

--- linux.dart
+++ linux.dart
@@ -1,3 +1,9 @@
 void main(){
   print("this file was created on my linux machine and used to test my git functionality");
+
+  // fine the first push went well
+  // let's try another one
+  String name = 'a';
+  print(a);
+  
 }
